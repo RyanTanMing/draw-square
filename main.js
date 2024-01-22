35cf71a0761604d55913a2b6e47944688bd12dc8
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2024-01-22 16:56:41 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+nosex=0,nosey=0
+rightwristx=0
+leftwristx=0
+difference=0
+function setup() {
+    video=createCapture(VIDEO)
+    video.size(550,500)
+    canvas=createCanvas(600,600)
+    canvas.position(700,150)
+    pn=ml5.poseNet(video,model_loaded)
+    pn.on('pose',got_poses)
+}
+function draw() {
+    background('cyan')
+    document.getElementById("square_side").innerHTML="Width and height of the square will be: "+difference+"px"
+    fill('green')
+    stroke('red')
+    square(nosex,nosey,difference)
+}
+function model_loaded() {
+    console.log("posenet has been initialnised")
+}
+function got_poses(results) {
+    if (results.length>0) {
+        //console.log(results)
+        nosex=results[0].pose.nose.x
+        nosey=results[0].pose.nose.y
+        leftwristx=results[0].pose.leftWrist.x
+        rightwristx=results[0].pose.rightWrist.x
+        difference=Math.floor(leftwristx-rightwristx)
+        console.log(difference)
+    }
+}
